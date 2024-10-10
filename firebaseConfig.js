648d410f568a256a5e0f637b619a3e37f44e018f
SagarKabariya <sagarhenry123@gmail.com>
2024-10-09 21:59:04 -0400

    fixing some firebase issue

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,14 @@
-// firebaseConfig.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyDvNJdZxVPF11f19C9ZeXpsJsxpbLaJruc",
-  authDomain: "rsvp-a4edd.firebaseapp.com",
-  projectId: "rsvp-a4edd",
-  storageBucket: "rsvp-a4edd.appspot.com",
-  messagingSenderId: "62941008363",
-  appId: "1:62941008363:web:88df281ea876afba5809ae",
-  measurementId: "G-PP0XP3R68L"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export { initializeApp, firebaseConfig, app };
