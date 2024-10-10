882d38596878d834878d37796aa2b5ce754a2df9
Sagar Kabariya <desk.sagar@gmail.com>
2024-10-09 20:38:00 -0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvNJdZxVPF11f19C9ZeXpsJsxpbLaJruc",
+  authDomain: "rsvp-a4edd.firebaseapp.com",
+  projectId: "rsvp-a4edd",
+  storageBucket: "rsvp-a4edd.appspot.com",
+  messagingSenderId: "62941008363",
+  appId: "1:62941008363:web:88df281ea876afba5809ae",
+  measurementId: "G-PP0XP3R68L"
+};
